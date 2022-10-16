3e48af3be3cf3db306bba55a47a3d1bcfd9be674
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-15 18:46:29 -0600

    complete model relationships

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 RSpec.describe Transaction, type: :model do
   describe 'relationships' do
-    it { should have_many :invoices }
+    it { should belong_to :invoice }
   end
 
 end
