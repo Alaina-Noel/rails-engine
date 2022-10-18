6f5028fdbc0252369b518e28dbbdda2778266153
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-18 11:33:39 -0600

    adds the ability to see a merchants items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :merchants, only: [:index, :show]
+      resources :merchants, only: [:index, :show] do
+        resources :items, only: [:index], to: 'merchant_items#index'
+      end
       resources :items  do
         resources :merchant, only: [:index], to: 'item_merchants#index'
       end
