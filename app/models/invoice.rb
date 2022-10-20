290e9e25f62c800fe776f39f353d5f422d6e8635
Alaina Kneiling <alaina.noel.8@gmail.com>
2022-10-20 13:36:08 -0600

    delete unnecessary comments

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -7,13 +7,5 @@ class Invoice < ApplicationRecord
 
   def self.delete_empty_invoices(array_of_invoice_ids)
     Invoice.left_joins(:invoice_items).select("invoices.*, count(invoice_items) as inv_item_count").having("count(invoice_items) = 0").where(id: array_of_invoice_ids).group(:id).destroy_all
-    #create a variable and delete that variable count for 1 - delete that variable.
-
-    # require 'pry' ; binding.pry
-    # Invoice.find(array_of_invoice_ids).each do |invoice|
-    #   if invoice.invoice_items.empty?
-    #     invoice.delete
-    #   end
-    # end
   end
 end
